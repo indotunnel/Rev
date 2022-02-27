e931aa61c7d290ede13c369a64028ec0d0033c41
Indotunnel <100355086+indotunnel@users.noreply.github.com>
2022-02-27 10:35:00 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo -e "=================================================" | lolcat
+echo -e "#         Premium Auto Script         #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By Mardhex                           #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "#                                      #"
+echo -e "# Allah                                 #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
